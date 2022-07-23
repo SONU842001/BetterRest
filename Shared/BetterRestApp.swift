7a42e5b670834770ec07c2e09b234e482bf435ca
SONU842001 <54150646+SONU842001@users.noreply.github.com>
2022-07-23 17:23:01 +0530

    Initial Commit

---
+++ Shared/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  Shared
+//
+//  Created by sonu kumar on 23/7/2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
